36c21d820a6a27a843cc67c64d946cfc9cf55226
QadeerHussain2303006KHIDEG <qadeer.hussain@xloopdigital.com>
2023-02-09 13:00:27 +0500

    route added
    route added

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from  flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def test():
     return "Test" 
+
+@app.route('/route_added')
+def route_added():
+    return "Qadeer Hussain" 
+
 
 if __name__ == "__main__":
     app.run(debug=True)
